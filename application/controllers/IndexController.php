03a2c0384accc7c81dc373dfa75d034901feddfd
unknown <Ken@ken-desktop.(none)>
2012-05-21 23:14:55 -0700

    Committing all sorts of scripting libraries, some application code, etc.

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,12 +5,13 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+        $this->view->headLink()->appendStylesheet('/styles/index/styles.css');
+		$this->view->headScript()->appendFile('/scripts/index/scripts.js');
     }
 
     public function indexAction()
     {
-        // action body
+
     }
 
 
